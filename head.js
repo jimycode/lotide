d4333406db43ee585eae9beda1e8e81652690225
Jameel <jameel.sameemi@gmail.com>
2021-11-22 22:42:35 -0700

    function inside function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("✅✅✅Assertion Passed: " + actual + "===" + expected);
+  } else {
+    return console.log("🛑🛑🛑Assertion Failed: " + actual + "!==" + expected);
+  }
+};
+  
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+
+let head = function(firstElement) {
+  return firstElement[0];
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
