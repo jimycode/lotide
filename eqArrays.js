c57d3afc296fbb8510b83040b69b8bf7c31bbee2
Jameel <jameel.sameemi@gmail.com>
2021-11-24 15:56:36 -0700

    eqArrays cleaner and with comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+//AssertEqual Function to use for test.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -5,20 +6,21 @@ const assertEqual = function(actual, expected) {
     return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-  
+//EqArrays function
+//This functions checks if both arrays are equal
 const eqArrays = function(array1,array2) {
   
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => should PASS
+// Test Cases. which takes in two arrays and returns true or false, based on a perfect match.
+
+assertEqual(eqArrays([1, 2, 3], [1, 2,3]),true); // => true
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false); // => false
 
