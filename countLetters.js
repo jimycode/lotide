44d723f7a03a451aa8e4ab499bf259ab1b29a27d
Jameel <jameel.sameemi@gmail.com>
2021-11-25 15:13:10 -0700

    more comments

--- countLetters.js
+++ countLetters.js
@@ -13,10 +13,10 @@ const assertEqual = function(actual, expected) {
 
 // the countletter function
 const countLetters = function(string) {
-    const countingResult = {};
-    for (let letters of string) { 
+    const countingResult = {}; // created an empty object to store the results of counted letters.
+    for (let letters of string) {  // using for of loop to iterate over the given string.
           if (countingResult[letters]) { 
-            countingResult[letters] += 1;
+            countingResult[letters] += 1; // adding the count if the letter exists.
         }else if (letters !== " "){ // ignore the space, from stake overflow.
             countingResult[letters] = 1;
         }
@@ -25,7 +25,7 @@ const countLetters = function(string) {
   }
 
   // Test case
-console.log(countLetters("Lighthouse in the house"));
-console.log(assertEqual((countLetters("LHL"))["L"], 2));
+console.log(countLetters("Lighthouse in the house")); // checks how many times a letter in the string.
+console.log(assertEqual((countLetters("LHL"))["L"], 2)); // using the assertEqual to see if the result passes.
 console.log(assertEqual((countLetters("lighthouse")) ["t"], 1));
 console.log(assertEqual((countLetters("lighthouse again")) ["a"], 1));
